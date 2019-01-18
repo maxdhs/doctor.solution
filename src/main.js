e88f64b0a790b9b374530d0d17ad840d2232e0d9
Maxwell Dubin <maxdhs@gmail.com>
2019-01-18 11:40:56 -0800

    Add test.json file with sample api response

--- src/main.js
+++ src/main.js
@@ -1,29 +1,40 @@
 // import { Player } from "./player";
 
+const buttonDiv = document.getElementById("button");
+const inputDiv = document.getElementById("userInput");
+buttonDiv.addEventListener("click", captureInput);
+let doctorName;
 exports.apiKey = process.env.API_KEY;
 
-let doctorName = "carlos";
+function captureInput() {
+  doctorName = inputDiv.value;
 
-let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=ff63ee1b0ab5c21900d757d30010e4bb`;
+  let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=ff63ee1b0ab5c21900d757d30010e4bb`;
 
-
-let promise = new Promise(function (resolve, reject) {
-  let request = new XMLHttpRequest();
-  request.onload = function () {
-    if (this.status === 200) {
-      resolve(request.response);
-    } else {
-      reject(Error(request.statusText));
+  let promise = new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.onload = function () {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
     }
-  }
-  request.open("GET", url, true);
-  request.send();
-});
+    request.open("GET", url, true);
+    request.send();
+  });
 
-promise.then(function (response) {
-  let body = JSON.parse(response);
-  console.log(body);
- 
-});
+  promise.then(function (response) {
+    let body = JSON.parse(response);
+    for(let i = 0; i < body.data.length; i++) {
+      console.log(body.data[i]);
+    }
+  });
+
+}
+
+
+
+
 
 
