803713ee12ce2045f9dd53cbf7227947b4d3772c
Maxwell Dubin <maxdhs@gmail.com>
2019-01-19 09:57:53 -0800

    update README

--- README.md
+++ README.md
@@ -1,31 +1,18 @@
-#  _Super Galactic Age Calculator_
+#  _Doctor Finder_
 
-#### _Independent Project for Epicodus: JavaScript / Webpack - 1/11/2019_
+#### _Independent Project for Epicodus: JavaScript / API - 1/19/2019_
 
-#### By _**Kenny Wolfenberger**_
+#### By _**Maxwell Dubin**_
 
 ## Description
 
-_Create a program that calculates a user's age on other planets based on inputted date._
-
+_Create a web app that searches by doctor's name or medical condition_
 
 ### Specs
 | Spec | Input |  Expected Output  |
 | :-------------    | :------------- | :------------- | 
-| Program will accept a birth date input | "1998, 0, 1" |  January 1, 1998  |
-| Program will calculate age in years based on birth date input | "1998, 0, 1" |  "21 years old" |
-| Program will convert age in Earth years to age in Mercury years (.24 Earth years) | "21 Earth Years" |  "87.5 Mercury years"  |
-| Program will convert age in Earth years to age in Venus years (.62 Earth years) | "30 Earth Years" |  "48.4 Mercury years"  |
-| Program will convert age in Earth years to age in Mars years (1.88 Earth years) | "16 Earth Years" |  "8.5 Mars years"  |
-| Program will convert age in Earth years to age in Jupiter years (11.86 Earth years) | "45 Earth Years" |  "3.8 Jupiter years"  |
-| Program will convert age in Earth years to age in Saturn years (29.46 Earth years) | "27 Earth Years" |  "0.9 Saturn years"  |
-| Program will convert age in Earth years to age in Uranus years (84.01 Earth years) | "42 Earth Years" |  "0.5 Uranus years"  |
-| Program will convert age in Earth years to age in Pluto years (248.59 Earth years) | "42 Earth Years" |  "0.17 Pluto years"  |
-| Program will determine remaining life expectancy in Earth years | current age: "56" life expectancy "76" |  "20 Earth years"  |
-| Program will determine remaining life expectancy in for each planet | current age: "56" life expectancy "76" |  "32 Venus years", "1.7 Jupiter years"  |
-| If a user has already surpassed their life expectancy, program will calculate their years beyond expectancy | current age: "99" life expectancy "76" |  "-23 years"  |
-
-
+| Program will accept a name of a doctor | "Frank West" |  (list of doctors with matching name displayed)  |
+| Program will accept a medical condition | "cancer" |  (list of doctors who specialize in cancer treatment)  |
 
 ## Setup/Installation Requirements
 * _Clone Repository from Github._
@@ -33,8 +20,6 @@ _Create a program that calculates a user's age on other planets based on inputte
 * _Complete project build by typing "npm run build" in the terminal._
 * _Type "npm test" to run all tests._
 * _To run the program type "npm run start"._
-
-
 
 ## Technologies Used
 * _Javascript_
@@ -48,9 +33,8 @@ _Create a program that calculates a user's age on other planets based on inputte
 * _VS Code_
 * _GitHub_
 
-
 ### License
 
 *This software is licensed under the MIT license.*
 
-Copyright (c) 2018 ** _Kenny Wolfenberger **
+Copyright (c) 2018 ** _Maxwell Dubin **
--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@
 
 const buttonDiv = document.getElementById("button");
 const inputDiv = document.getElementById("userInput");
+const outputDiv = document.getElementById("results");
 buttonDiv.addEventListener("click", captureInput);
-let doctorName;
+
+const buttonDiv2 = document.getElementById("button2");
+const inputDiv2 = document.getElementById("userInput2");
+const outputDiv2 = document.getElementById("results2");
+buttonDiv2.addEventListener("click", captureInput2);
+
+let userInput;
+let userInput2;
 exports.apiKey = process.env.API_KEY;
 
 function captureInput() {
-  doctorName = inputDiv.value;
+  userInput = inputDiv.value;
 
-  let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=ff63ee1b0ab5c21900d757d30010e4bb`;
+  let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${userInput}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=ff63ee1b0ab5c21900d757d30010e4bb`;
 
   let promise = new Promise(function (resolve, reject) {
     let request = new XMLHttpRequest();
@@ -26,15 +34,57 @@ function captureInput() {
 
   promise.then(function (response) {
     let body = JSON.parse(response);
-    for(let i = 0; i < body.data.length; i++) {
-      console.log(body.data[i]);
+    if (body.meta.total == 0) {
+      outputDiv.innerHTML = "No doctors found.";
     }
+    else {
+      outputDiv.innerHTML = "Your found doctors: <br><br>";
+      for (let i = 0; i < body.data.length; i++) {
+        for (let j = 0; j < body.data[i].practices.length; j++) {
+          outputDiv.innerHTML += "<li>" + body.data[i].practices[j].name + " <br>" + body.data[i].practices[j].visit_address.street + ", " + body.data[i].practices[j].visit_address.city + ", " + body.data[i].practices[j].visit_address.state + " " + "<br>Accepting new patients: " + body.data[i].practices[j].accepts_new_patients + "<br> Phone number:" + body.data[i].practices[j].phones[0].number + "<br> Website: " + body.data[i].practices[j].website + "<br><br></li>";
+        }
+      }
+    }
+  });
+
+  promise.catch(function (error) {
+    outputDiv.innerHTML = "API returned error: " + error;
   });
 
 }
 
+function captureInput2() {
+  userInput2 = inputDiv2.value;
 
+  let url = `https://api.betterdoctor.com/2016-03-01/practices?name=${userInput2}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=ff63ee1b0ab5c21900d757d30010e4bb`;
 
+  let promise = new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.onload = function () {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
 
+  promise.then(function (response) {
+    let body = JSON.parse(response);
+    if (body.meta.total == 0) {
+      outputDiv2.innerHTML = "No doctors found to treat your condition";
+    }
+    else {
+      outputDiv2.innerHTML = "Doctors available to treat your condition: <br>";
+      for (let i = 0; i < body.data.length; i++) {
+        outputDiv2.innerHTML += "<li>" + body.data[i].name + " " + body.data[i].name + "</li>";
+      }
+    }
+  });
 
-
+  promise.catch(function (error) {
+    outputDiv2.innerHTML = "API returned error: " + error;
+  });
+}
